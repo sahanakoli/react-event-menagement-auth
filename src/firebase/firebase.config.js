f23444e15b47548422b4c6f526cb3cbbf7b9a914
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-06 23:18:07 +0600

    router setup and added firebase config js

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMzyAg3qcxbfJpt9s9AZVgPxPgjX8znUM",
+  authDomain: "react-event-management-a-40591.firebaseapp.com",
+  projectId: "react-event-management-a-40591",
+  storageBucket: "react-event-management-a-40591.appspot.com",
+  messagingSenderId: "1079417501103",
+  appId: "1:1079417501103:web:c818a826f23e1525cf6b5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
